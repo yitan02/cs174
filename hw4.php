e8d0f1df649a2f5e52c790d8cb71ff93919c9831
Yinglin Tan <90019109+yitan02@users.noreply.github.com>
2023-10-19 10:29:23 -0700

    Update hw4.php

--- hw4.php
+++ hw4.php
@@ -1,16 +1,20 @@
 <?php
+    //questions
+    //how to test for connection error?
+    //do we need to make a class?
+    //fix_String function
+    //can we use file_get_contents?
+    //where to close result?
+
     require_once 'login.php';
-    mysqli_report(MYSQLI_REPORT_OFF);
 
-    $conn = @new mysqli($hn,$un,$pw,$db);
-    if($conn->connect_error)
-        die (mysql_fatal_error("Connection failed"));
+    define("ERROR_MSG", "Please try again.");
+    define("COL_SIZE",2);
 
-    // try {
-    //     $conn = new mysqli($hn,$un,$pw,$db);
-    // } catch {
-    //     die (mysql_fatal_error("connection failed"));
-    // }
+    $conn = new mysqli($hn,$un,$pw,$db);
+
+    if($conn->connect_error) die (ERROR_MSG);
+
 
     echo <<<_END
     <html>
@@ -18,53 +22,21 @@
     <h1>Homework 4</h1>
     <form method = "post" action="hw4.php" enctype="multipart/form-data">
         <label for ="title">Title:</label>
-        <input type="text" id="title name="title><br><br>
+        <input type="text" id="title" name="title"><br><br>
         <label for="file"> Select File: </label><br>
         <input id="file" type="file" name="filename" size="10"><br><br>
         <button>Submit</button>
     </form>
     _END;
 
-    $query = "CREATE TABLE IF NOT EXISTS cs (
-        id INT NOT NULL AUTO_INCREMENT,
-        title VARCHAR(32),
-        content VARCHAR(1000),
-        PRIMARY KEY (id)
+    $query = "CREATE TABLE IF NOT EXISTS hw4 (
+        title VARCHAR(24),
+        content VARCHAR(2000)
         )";
     $result = $conn->query($query);
 
-    // $name = "jshfdsjkh";
-    // $query ="INSERT INTO test(name,age) VALUES ('$name',10)";
-    // $result = $conn->query($query);
-
-    // $query = "INSERT INTO test(name,age) VALUES('bob',20)";
-    // $result = $conn->query($query);
-
-    // $query = "INSERT INTO test(name,age) VALUES('maria',19)";
-    // $result = $conn->query($query);
-
-    // $query = "DELETE FROM test WHERE name='maria'";
-    // $result = $conn->query($query);
-
-    // $query ="SELECT * FROM test";
-    // $result = $conn->query($query);
-
-    // $rows = $result->num_rows;
-    // echo "<table><tr> <th>Id</th><th>Name</th><th>Age</th></tr>";
-    // for ($j = 0 ; $j < $rows ; ++$j)
-    // {
-    //     $result->data_seek($j);
-    //     $row = $result->fetch_array(MYSQLI_NUM);
-    //     echo "<tr>";
-    //     for ($k = 0 ; $k < 3 ; ++$k) echo "<td>$row[$k]</td>";
-    //     echo "</tr>";
-    // }
-    // echo "</table>";
-    
-
-    // //array to hold the lines read from the file
-    // $file_lines = array();
-
+    //array to hold the lines read from the file
+    $file_lines = array();
 
     if ($_FILES){
         $file_line = "";
@@ -80,31 +52,25 @@
                 if($line !== false){
                     //sanitize the line
                     $line = htmlentities($line, ENT_QUOTES, 'UTF-8');
+                    
                     $file_line .= $line;
                 }
             }
             //close the file
             fclose($file);
 
-            //echo $file_line;
+            $title = mysql_entities_fix_string($conn,$_POST['title']);
 
-            // $query = "INSERT INTO cs(title,content) VALUES('Hello', '$file_line')";
-            // $result = $conn->query($query);
-
-            $query = "SELECT * FROM cs";
-            $result = $conn->query($query);
-
-            $rows = $result->num_rows;
-            echo "<table><tr> <th>Id</th><th>Title</th><th>Content</th></tr>";
-            for ($j = 0 ; $j < $rows ; ++$j)
-            {
-                $result->data_seek($j);
-                $row = $result->fetch_array(MYSQLI_NUM);
-                echo "<tr>";
-                for ($k = 0 ; $k < 3 ; ++$k) echo "<td><br>$row[$k]</td>";
-                echo "</tr>";
+            //Check empty title
+            if(!$title){
+                echo "Title is empty";
             }
-            echo "</table>";
+            // else{
+            //     storeData($title,$file_lines,$conn);
+            // }
+
+            printOutTable($conn);
+
         }
         //print out error message if file was unable to open
         else {
@@ -113,20 +79,44 @@
     }
     echo "</body></html>";
 
-    function mysql_fatal_error($msg){
-        echo $msg;
+    function mysql_entities_fix_string($conn,$string){
+        return htmlentities(mysql_fix_string($conn,$string));
     }
 
-    class FindPrimes{
+    function mysql_fix_string($conn,$string){
+        //if(get_magic_quotes_gpc()){                   //this is no longer available in php ver 8
+            $string = stripslashes($string);
+        //}
+        return $conn->real_escape_string($string);
+    }
 
+    function storeData($title,$file_lines,$conn){
+        $query = "INSERT INTO hw4 VALUES('$title','$file_lines')";
+        $result = $conn->query($query);
+        if(!$result) die (ERROR_MSG);
+    }
 
+    function printOutTable($conn){
+        $query = "SELECT * FROM hw4";
+        $result = $conn->query($query);
 
+        $rows = $result->num_rows;
+        echo "<table><tr><th>Title</th><th>Content</th></tr>";
+        for ($j = 0 ; $j < $rows ; ++$j)
+        {
+            $result->data_seek($j);
+            $row = $result->fetch_array(MYSQLI_NUM);
+            echo "<tr>";
+            for ($k = 0 ; $k < COL_SIZE ; ++$k) echo "<td><br>$row[$k]</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
 
-
+        //close connection
+        $conn->close();
 
     }
 
-
-
-
+    //close connection
+    //$conn->close();
 ?>
