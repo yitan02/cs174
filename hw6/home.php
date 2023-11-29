805c7e1905d7bba0d9411235394dea710b613d83
Yinglin Tan <90019109+yitan02@users.noreply.github.com>
2023-11-29 12:43:38 -0800

    Create home.php

---
+++ hw6/home.php
@@ -0,0 +1,142 @@
+<?php
+    session_start();
+    require_once 'login.php';
+
+    define("COL_SIZE",2); 
+    define('WEEK_IN_SEC', 60 * 60 * 24 * 7);
+    define("START_CHAR", 0);
+    define("END_CHAR", 100);
+
+    //create new mysql connection
+    $conn = new mysqli($hn,$un,$pw,$db);
+
+    //check if error connection 
+    if($conn->connect_error) die (printError());
+
+    //check if there's a session
+    if(isset($_SESSION['student_id'])){
+        $id = mysql_entities_fix_string($conn, $_SESSION['student_id']);
+        $name = mysql_entities_fix_string($conn, $_SESSION['name']);
+
+        echo <<<_END
+        <html>
+        <body>
+        <link rel="stylesheet" type="text/css" href="style.css">
+ 
+        <title>Home</title></head>
+        <h1>Hello $name! </h1>
+        <form method="post" action="">
+            <input type="submit" name="logout" value="Logout">
+        </form>
+
+        <div class="upload-form">
+            <form method = "post" action="home.php">
+                <label for ="name">Name:</label>
+                <input type="text" id="name" name="name"><br><br>
+
+                <label for ="id">Student ID (last two digits):</label>
+                <input type="text" id="id" name="id"><br><br>
+
+                <button>Submit</button>
+            </form>
+        </div>
+
+        _END;
+        echo "</body></html>";
+
+        if(!empty($_POST['id']) && !empty($_POST['name'])){
+            
+        }
+
+    }
+    else{
+        echo "Please <a href='main.php'> click here</a> to log in.";
+        $conn->close();
+    }
+
+    //print table if session is active
+    if(isset($_SESSION['student_id'])){
+        $id = mysql_entities_fix_string($conn, $_SESSION['student_id']);
+
+        printThreads($id, $conn);
+    }
+
+    //destroy session if logout button is clicked
+    if(isset($_POST['logout'])){
+        destroy_session_and_data();
+    }
+
+    //generic error msg function
+    function printError(){
+        echo "Please try again.";
+    }
+
+    //sanitize user input
+    function mysql_entities_fix_string($conn,$string){
+        return htmlentities(mysql_fix_string($conn,$string));
+    }
+
+    //sanitize user input
+    function mysql_fix_string($conn,$string){
+        return $conn->real_escape_string($string);
+    }
+
+    //insert thread name and file lines to the database
+    function storeData($user_id,$thread_name,$file_lines,$conn){
+        $query = "INSERT INTO threads (user_id, thread_name,content) VALUES('$user_id','$thread_name','$file_lines')";
+        $result = $conn->query($query);
+
+        //check if query failed
+        if(!$result){
+            printError();
+        }
+        else{
+            echo "Insert success!";
+        }
+    }
+
+    //prints out the data table
+    function printThreads($user_id,$conn){
+        $query = "SELECT thread_name,content FROM threads WHERE user_id ='$user_id'";
+        $result = $conn->query($query);
+
+        $rows = $result->num_rows;
+        echo "<table>
+                <tr>
+                    <th class='thread-name-column'>Thread Name</th>
+                    <th class='content-column'>Content</th>
+                </tr>";
+        for ($j = 0 ; $j < $rows ; ++$j)
+        {
+            $result->data_seek($j);
+            $row = $result->fetch_array(MYSQLI_NUM);
+
+            //truncate the strings if expand button is not set
+            if(!isset($_POST['Expand'])){
+                $row[1] = substr($row[1], START_CHAR, END_CHAR);
+            }
+
+            echo "<tr>";
+            for ($k = 0 ; $k < COL_SIZE ; ++$k) 
+                echo "<td><br>$row[$k]</td>";
+
+            echo "</tr>";
+        }
+        echo "</table>";
+
+        //close result
+        $result->close();
+
+        //close connection
+        $conn->close();
+
+    }
+
+    //destroy session
+    function destroy_session_and_data(){
+        $_SESSION = array();
+        setcookie(session_name(),'',time() - WEEK_IN_SEC, '/');
+        session_destroy();
+        header("Location: /cs174/hw6/main.php");
+    }
+?>
